3cf4e99b9b757aee477645d135eaa162e9fe6f3e
Dan <danvolosnikov@gmail.com>
2016-04-28 20:52:21 +0000

    cleaned clementine

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,20 +1,6 @@
 'use strict';
-
-var path = process.cwd();
-
 module.exports = function (app) {
-
-	function isLoggedIn (req, res, next) {
-			return next();
-		}
-
-	app.route('/')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/index.html');
-		});
-
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
+	app.route('/').get(function (req, res) {
+			res.sendFile(process.cwd() + '/public/index.html');
 		});
 };
